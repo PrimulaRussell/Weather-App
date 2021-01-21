ecfd9bd69178c7f38e1f5087a01f088be50ad4e2
primula <primularussell@gmail.com>
2021-01-20 20:13:29 -0700

    created private key location

---
+++ config.js
@@ -0,0 +1,3 @@
+var config = {
+    WEATHER_API_TOKEN: '9bfc5e225e729951da77a3d983278acd',
+}
--- js/main.js
+++ js/main.js
@@ -1,32 +1,41 @@
-var submit = document.querySelector(".submit")
-var inputValue = document.querySelector(".inputValue")
-var notSearched = document.querySelector(".notSearched")
-var searched = document.querySelector(".searched")
-var city = document.querySelector(".city")
-var temp = document.querySelector(".temp")
-var feeling = document.querySelector(".feeling")
-var wind = document.querySelector(".wind")
-var humidity = document.querySelector(".humidity")
+//Declaring variables//
+var submit = document.querySelector(".submit");
+var inputValue = document.querySelector(".inputValue");
+var notSearched = document.querySelector(".notSearched");
+var searched = document.querySelector(".searched");
+var city = document.querySelector(".city");
+var temp = document.querySelector(".temp");
+var feeling = document.querySelector(".feeling");
+var wind = document.querySelector(".wind");
+var humidity = document.querySelector(".humidity");
+
+//links to private api key//
+var apiKey = config.WEATHER_API_TOKEN;
+
+//fetches weather data when submit button is clicked//
 
 submit.addEventListener('click', function() {
-    fetch('api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=')
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&APPID=' + apiKey)
         .then(response => response.json)
         .then(changeDisplay())
-        .then(data => {
-            var cityValue = data['name'];
-            var tempValue = data['main']['temp'];
-            var feelingValue = data['main']['feels_like'];
-            var windValue = data['wind'];
-            var humidityValue = data['main']['humidity'];
 
-            city.innerHTML = cityValue;
-            temp.innerHTML = tempValue;
-            feeling.innerHTML = feelingValue;
-            wind.innerHTML = windValue;
-            humidity.innerHTML = humidityValue;
-        })
+    //gets data from weather api and displays it//
+    .then(data => {
+        var cityValue = data['name'];
+        var tempValue = data['main.temp'];
+        var feelingValue = data['main.feels_like'];
+        var windValue = data['wind'];
+        var humidityValue = data['main.humidity'];
+
+        city.innerHTML = cityValue;
+        temp.innerHTML = tempValue;
+        feeling.innerHTML = feelingValue;
+        wind.innerHTML = windValue;
+        humidity.innerHTML = humidityValue;
+    })
 })
 
+//changes display to show weather stats when submit is clicked//
 function changeDisplay() {
     notSearched.style.display = "none";
     searched.style.display = "initial";
