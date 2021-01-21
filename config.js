ce7ae9dc3f30d2cda0411f6650550693f21c2d9c
primula <primularussell@gmail.com>
2021-01-21 13:24:09 -0700

    created git ignore, made weather data display

--- config.js
+++
@@ -1,3 +0,0 @@
-var config = {
-    WEATHER_API_TOKEN: '9bfc5e225e729951da77a3d983278acd',
-}
--- js/main.js
+++ js/main.js
@@ -8,32 +8,51 @@ var temp = document.querySelector(".temp");
 var feeling = document.querySelector(".feeling");
 var wind = document.querySelector(".wind");
 var humidity = document.querySelector(".humidity");
-
+var far = document.querySelector(".far");
+var cel = document.querySelector(".cel");
 //links to private api key//
-var apiKey = config.WEATHER_API_TOKEN;
+var apiKey = config.WEATHER_API_KEY;
+
+//Changes units from Fahrenheit  to celcius when clicked//
+
+//Coming back to this when I have learned React//
+/*
+$("button").click(function() {
+    $(".unit").toggleClass("bold");
+
+    if ($(".cel").hasClass("bold")) {
+        (parseInt(temp.innerHTML) - 30) * 5 / 9
+    }
+});
+*/
+
+
 
 //fetches weather data when submit button is clicked//
 
-submit.addEventListener('click', function() {
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&APPID=' + apiKey)
-        .then(response => response.json)
+submit.addEventListener("click", function() {
+    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&units=imperial&APPID=" + apiKey)
+        .then(response => response.json())
         .then(changeDisplay())
 
     //gets data from weather api and displays it//
     .then(data => {
-        var cityValue = data['name'];
-        var tempValue = data['main.temp'];
-        var feelingValue = data['main.feels_like'];
-        var windValue = data['wind'];
-        var humidityValue = data['main.humidity'];
+        var cityValue = data.name;
+        var tempValue = Math.round(data.main.temp);
+        var feelingValue = Math.round(data.main.feels_like);
+        var windValue = data.wind.speed;
+        var humidityValue = data.main.humidity;
 
         city.innerHTML = cityValue;
-        temp.innerHTML = tempValue;
-        feeling.innerHTML = feelingValue;
-        wind.innerHTML = windValue;
-        humidity.innerHTML = humidityValue;
+        temp.innerHTML = tempValue + "°";
+        feeling.innerHTML = "Feels like: " + feelingValue + "°";
+        wind.innerHTML = "Wind: " + windValue + " mph";
+        humidity.innerHTML = "Humidity: " + humidityValue;
     })
+
+    .then(inputValue.value = "")
 })
+
 
 //changes display to show weather stats when submit is clicked//
 function changeDisplay() {
